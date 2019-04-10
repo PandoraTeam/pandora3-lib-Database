5e09cad1ee23beac093e3a0b1f5246d3ee48782b
NesCafe62 <U.238@rambler.ru>
2019-04-10 23:23:54 +0200

    Update DatabaseConnection.php

--- DatabaseConnection.php
+++ DatabaseConnection.php
@@ -1,13 +1,50 @@
 <?php
-namespace pandora3\libs\Database;
+namespace Pandora3\Libs\Database;
 
-use pandora3\core\Storage\Database\IDatabaseConnection;
+use Pandora3\Libs\Database\Exception\ConnectionFailedException;
+use PDO;
+use PDOException;
+use Pandora3\Core\Interfaces\DatabaseConnectionInterface;
 
-class DatabaseConnection implements IDatabaseConnection {
+class DatabaseConnection implements DatabaseConnectionInterface {
 
-	public function __construct(array $connectionParams) {
-		// $connectionParams;
+	/** @var array $params */
+	protected $params;
+
+	/** @var PDO $connection */
+	protected $connection;
+
+	public function __construct(array $params) {
+		$this->params = $params;
+		$this->connect();
 	}
 
+	public function connect(): void {
+		$driver = $this->params['driver'] ?? 'mysql';
+		$host = $this->params['host'] ?? 'localhost';
+		$database = $this->params['database'];
+		$username = $this->params['username'];
+		$password = $this->params['password'];
+		$charset = $this->params['charset'] ?? 'utf8';
+		// $collation = $this->params['collation'] ?? 'utf8_unicode_ci';
+		// $prefix = $this->params['prefix'] ?? '';
+
+		try {
+			$this->connection = new PDO("$driver:host=$host;dbname=$database", $username, $password, [
+				PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+				PDO::MYSQL_ATTR_INIT_COMMAND => "set names $charset",
+			]);
+		} catch (PDOException $ex) {
+			throw new ConnectionFailedException('Failed to connect database', E_ERROR, $ex);
+		}
+	}
+
+	public function close(): void {
+		$this->connection = null;
+	}
+
+	public function getConnection(): PDO {
+		return $this->connection;
+	}
 
 }
